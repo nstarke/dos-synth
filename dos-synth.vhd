92fd1376244b78a0420d96ad0b13780ded8c7ae0
nstarke <starke.nicholas@gmail.com>
2025-09-21 20:11:01 -0500

    Adding VR.BAT and adjusting configuration so MIDI will work with 'vr_demo' synth

--- dos-synth.vhd
+++ dos-synth.vhd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b495f453aad47999abcdbce5d2d47039b169e6c8ebcb7cd1e41ce2c0134c18f4
+oid sha256:43155bc05ee98a08c312b24d30f05030fa237a4e5b44bac1ce65b174c5eca2d9
 size 340241920
